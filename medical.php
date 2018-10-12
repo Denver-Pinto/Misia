14f484500f44acdff8dae89dd72803ee67f5e2a9
Denver-Pinto <37263968+Denver-Pinto@users.noreply.github.com>
2018-10-12 22:53:11 +0530

    google map api

--- medical.php
+++ medical.php
@@ -1,6 +1,18 @@
 <?php
 require 'connection.php';
 session_start();
+$lat= $_POST["lat"];
+$lng=$_POST["lng"];
+$product=$_POST["medicine"];
+$number=0;
+$distance=$_POST["distance"];
+echo  $lat."<br>";
+echo $lng."<br>";
+echo $product."<br>";
+echo $distance."<br>";
+$lat_array=array($lat);
+$lng_array=array($lng);
+$information =array("you");
 ?>
 <html>
 <head>
@@ -13,21 +25,24 @@ $(document).ready(function(){
 
 });
 </script>
+
+
 </head>
 <body>
+<script>
+function myMap() {
+var marker;
+var latlng;
+var window;
+var mapProp= {
+    center:new google.maps.LatLng(<?php echo $lat  ?>,<?php echo $lng  ?>),
+    zoom:12,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 <?php
-//$_SESSION["POST_VARS"]=$_POST;
-$lat= $_POST["lat"];//$_SESSION["POST_VARS"]["lat"];
-$lng=$_POST["lng"];//$_SESSION["POST_VARS"]["lng"];
-$product=$_POST["medicine"];//$_SESSION["POST_VARS"]["medicine"];
-$number=0;
-$distance=$_POST["distance"];//$_SESSION["POST_VARS"]['distance'];
-echo  $lat."<br>";
-echo $lng."<br>";
-echo $product."<br>";
-echo $distance."<br>";
+
 /* create a prepared statement*/
-$select_distance_preparedstmt =mysqli_prepare($con, "SELECT  ( 6371 * acos( cos( radians(?) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(?) ) + sin( radians(?) ) * sin( radians( lat ) ) ) ) AS distance,username FROM shops  HAVING distance< ? ORDER BY distance LIMIT 0 , 20");
+$select_distance_preparedstmt =mysqli_prepare($con, "SELECT  ( 6371 * acos( cos( radians(?) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(?) ) + sin( radians(?) ) * sin( radians( lat ) ) ) ) AS distance,username,lat,lng FROM shops  HAVING distance< ? ORDER BY distance LIMIT 0 , 20");
 
 if ( !$select_distance_preparedstmt ) {
   die('mysqli error: '.mysqli_error($con));
@@ -40,6 +55,7 @@ if ( !mysqli_execute($select_distance_preparedstmt) ) {
     /* execute query */
     mysqli_stmt_execute($select_distance_preparedstmt);
 	$result = mysqli_stmt_get_result($select_distance_preparedstmt);
+	//got the nearest stores from the given latitude and longitude
      $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
 		
@@ -48,8 +64,7 @@ if ( !mysqli_execute($select_distance_preparedstmt) ) {
     mysqli_stmt_close($select_distance_preparedstmt);
 	   while($row)
 	 {
-		// echo $row["username"];
-       
+       //for each store checking if the product exists 
 	$select_product_preparedstmt= mysqli_prepare($con, "SELECT price from {$row["username"]} where product=?");
 	    //check the prepared statement
 		if ( !$select_product_preparedstmt ) {
@@ -65,14 +80,16 @@ if ( !mysqli_execute($select_distance_preparedstmt) ) {
 		//bind variables to prepared statement 
 		mysqli_stmt_bind_result($select_product_preparedstmt, $price);
 		if(mysqli_stmt_fetch($select_product_preparedstmt)) {
-        echo " <div>Product exists at {$row["username"]}at price".$price."</div>";
-		$number++;}
-		else{
-			//echo"Product doesnt exist<br>";
+        //echo " <div>Product exists at {$row["username"]}at price".$price."</div>";
+		$number++;
+		array_push($lat_array,$row["lat"]);
+		array_push($lng_array,$row["lng"]);
+		array_push($information,$row["username"]."<br>"."Rs.".$price);
 		}
 		  // close statement 
 		mysqli_stmt_close($select_product_preparedstmt);
 		$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+		
 	 
     }
 
@@ -92,7 +109,25 @@ if ( !mysqli_execute($insert_data_preparedstmt) ) {
    mysqli_stmt_close($insert_data_preparedstmt);    
 /* close connection */
 mysqli_close($con);
+while($number>=0)
+{	
 ?>
+latlng = new google.maps.LatLng(<?php echo array_pop($lat_array);?>,<?php echo array_pop($lng_array);?>);
+marker = new google.maps.Marker({position: latlng,
+animation:google.maps.Animation.BOUNCE});     
+marker.setMap(map);
+infowindow = new google.maps.InfoWindow({
+   content: "<?php echo array_pop($information);?>"
+ });
+ infowindow.open(map,marker);
+ 
+<?php 
+$number--;}?>
+}
+</script>
 <button>Search something else</button>
+<div id="googleMap" style="width:100%;height:400px;"></div>
+<script src="https://maps.googleapis.com/maps/api/js?key=&callback=myMap"></script>
 </body>
 </html>
+
